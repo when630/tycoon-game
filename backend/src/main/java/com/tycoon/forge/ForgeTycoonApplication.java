b90f03539326df3203a34cf9886167aeee397bb5
forcs <forcs@localhost>
2026-02-02 10:23:30 +0900

    초기 설정: Docker 환경 및 Backend 구성

---
+++ backend/src/main/java/com/tycoon/forge/ForgeTycoonApplication.java
@@ -0,0 +1,13 @@
+package com.tycoon.forge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgeTycoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgeTycoonApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tycoon/forge/ForgeTycoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tycoon.forge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgeTycoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
